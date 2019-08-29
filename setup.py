25cb533635acfb5c7bbede35c6171d31b549f0be
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2019-08-28 21:45:46 -0700

    ensured babel working

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     version="1.0.0",
     author="Matthew Hennefarth",
     packages=find_packages(),
-    package_data={'': ['.json', '*.j2', '*.config']}
+    package_data={'': ['.json', '*.j2', '*.config']},
+    scripts=['dmdpy/setupjob.py']
 )
