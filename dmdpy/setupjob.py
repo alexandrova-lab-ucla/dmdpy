4baf55195e4cd4d790cd8f446e93b73407b341cb
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2019-10-01 15:27:45 -0700

    Still some issues, but it seems dependent on dmd type...right now it is working with the DMD i am using

--- dmdpy/setupjob.py
+++ dmdpy/setupjob.py
@@ -5,6 +5,7 @@ import logging
 import json
 import pkg_resources
 import shutil
+import subprocess
 from subprocess import Popen, PIPE
 
 
@@ -15,12 +16,6 @@ logger = logging.getLogger(__name__)
 
 os.environ["PATH"] += os.pathsep + "/home/mhennefarth/Programs/dmd/bin"
 
-"""
-1) Reformat pdb as best as I can
-2) Generate input files
-3) Run quick step of DMD to ensure it works
-
-"""
 class setupDMDjob:
 
     # Have it do a small DMD step after everything is donew
@@ -122,11 +117,66 @@ class setupDMDjob:
         self.make_topparam()
         self.make_inConstr()
         self.make_state_file()
+        self.short_dmd()
         self.make_start_file()
 
-        # TODO Have it run a quick DMD step to ensure it works properly
-        # Can also use this to reformat the pdb!
-        # But then, we need to figure out how to link the initial.pdb to the new.pdb (output from single step of dmd)
+    def short_dmd(self):
+        try:
+            with open("dmd_start_short", 'w') as dmdstart:
+                dmdstart.write(f"THERMOSTAT     {self._raw_parameters['Thermostat']}\n")
+                dmdstart.write(f"T_NEW          0.001\n")
+                dmdstart.write(f"T_LIMIT        0.001\n")
+                dmdstart.write(f"HEAT_X_C       1\n")
+                dmdstart.write(f"RESTART_FILE   {self._raw_parameters['Restart File']}\n")
+                dmdstart.write(f"RESTART_DT     1\n")
+                dmdstart.write(f"ECHO_FILE      {self._raw_parameters['Echo File']}\n")
+                dmdstart.write(f"ECHO_DT        1\n")
+                dmdstart.write(f"MOVIE_FILE     {self._raw_parameters['Movie File']}\n")
+                dmdstart.write(f"START_TIME     0\n")
+                dmdstart.write(f"MOVIE_DT       1\n")
+                dmdstart.write(f"MAX_TIME       5\n")
+
+        except IOError:
+            logger.exception("Error writing out dmd_start file")
+            raise
+
+        # Here we do a short run
+        # TODO wrap in some try/except clauses
+        with Popen(
+                f"pdmd.linux -i dmd_start_short -s state -p param -c outConstr -m 1",
+                stdout=PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True, env=os.environ) as shell:
+            while shell.poll() is None:
+                logger.debug(shell.stdout.readline().strip())
+
+        if not os.path.isfile("movie"):
+            logger.error("movie file was not made, dmd seems to be anrgy at your pdb")
+            raise ValueError("initial.pdb")
+
+        logger.debug("Finished the short DMD step successfully")
+
+        with Popen(
+                f"complex_M2P.linux {self._dmd_config['PATHS']['parameters']} initial.pdb topparam movie check.pdb inConstr",
+                stdout=PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True, shell=True, env=os.environ) as shell:
+            while shell.poll() is None:
+                logger.debug(shell.stdout.readline().strip())
+
+        if not os.path.isfile("check.pdb"):
+            logger.error("check.pdb not found, complex_M2P.linux did not run properly")
+            raise ValueError("check.pdb")
+
+        else:
+            with open("check.pdb") as pdb:
+                if len(pdb.readlines()) == 0:
+                    logger.error("check.pdb is empty, complex_M2P.linux did not run properly")
+                    raise ValueError("check.pdb")
+
+        logger.debug("Was able to create a pdb from the short DMD run")
+        logger.debug("Good to go, removing old file now")
+        os.remove("check.pdb")
+        os.remove(self._raw_parameters['Movie File'])
+        os.remove(self._raw_parameters['Echo File'])
+        os.remove(self._raw_parameters['Restart File'])
+        os.remove("dmd_start_short")
 
     def make_topparam(self):
         try:
@@ -203,11 +253,62 @@ class setupDMDjob:
             # It is coming from the mol2 of the substrate...interesting...
             # There is a Segmentation fault (core dumped) error that occurs, asking Jack if he knows what the issue is
             # Jack thinks it is the segfault mike wrote in his HACK ALERT section
-            with Popen(f"complex.linux -P {self._dmd_config['PATHS']['parameters']} -I {self._protein.name} -T topparam -D 200 -p param -s state -C inConstr -c outConstr",
-                       stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True, env=os.environ) as shell:
+            # I have emailed the Dohkyan group regarding it...its only for certain pdbs...
+            with Popen(f"complex-1.linux -P {self._dmd_config['PATHS']['parameters']} -I {self._protein.name} -T topparam -D 200 -p param -s state -C inConstr -c outConstr",
+                       stdout=PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True, env=os.environ) as shell:
                 while shell.poll() is None:
                     logger.debug(shell.stdout.readline().strip())
-                    logger.debug(shell.stderr.readline().strip())
+
+            attempts = 3
+            if not os.path.isfile("state"):
+                logger.warning("Could not make state file first time around, could be a segfault error")
+                logger.warning("Going to reorder the bond list in the mol2 files!")
+                attempts = 1
+
+            while not os.path.isfile("state") and attempts < 3:
+                logger.warning(f"complex fix attempt {attempts}")
+                mol2_files = []
+                with open("topparam") as topparm:
+                    for line in topparm:
+                        mol2_files.append(line.split()[2])
+
+                for mol2 in mol2_files:
+                    with open(mol2, 'r') as mf:
+                        bonds = []
+                        save = []
+                        bond_section = False
+                        for line in mf:
+                            if not bond_section and "BOND" in line:
+                                save.append(line)
+                                bond_section = True
+                                continue
+
+                            if bond_section:
+                                bonds.append(line.split())
+
+                            else:
+                                save.append(line)
+
+                        #now we reorder based upon the attempt!
+                        bonds.sort(key=lambda x: int(x[attempts]))
+
+                    with open(mol2, 'w+') as mf:
+                        for line in save:
+                            mf.write(line)
+
+                        for bond in bonds:
+                            mf.write(f"{bond[0]}\t{bond[1]}\t{bond[2]}\t{bond[3]}\n")
+
+                    with Popen(f"complex-1.linux -P {self._dmd_config['PATHS']['parameters']} -I {self._protein.name} -T topparam -D 200 -p param -s state -C inConstr -c outConstr",
+                            stdout=PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True, env=os.environ) as shell:
+                        while shell.poll() is None:
+                            logger.debug(shell.stdout.readline().strip())
+
+                attempts += 1
+
+            if not os.path.isfile("state"):
+                logger.critical("could not create state file, something is very wrong!")
+                raise ValueError("state file")
 
         except OSError:
             logger.exception("Error calling complex-1.linux!")
